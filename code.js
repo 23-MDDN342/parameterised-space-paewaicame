efa104754f857ac088f7362ab91c29975ff23827
Cameron Paewai <paewaicame@myvuw.ac.nz>
2024-03-26 02:03:26 +1300

    Finished gradient-shifting background + beginning of color palettes and shape overlays

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 // these can be customized
 const debugViewText = "#ff0000";
-const debugZoomBackground = "#000"
+const debugZoomBackground = "#000";
 const debugZoomScale = 0.5;
 
 // this can be modified after we discuss in lecture
@@ -16,30 +16,58 @@ let stickFrame = 0;
 
 // *note: canvasWidth and canvasHeight will be defined before this script runs)
 
+
+/* extra functions for me */
 let imageNoise;
 function preload() {
     imageNoise = loadImage('noise.webp');
 }
 
-function setup () {
-    let main_canvas = createCanvas(canvasWidth,canvasHeight);
+// linear congruential generator because i'm not allowed to use Math.random >:D
+// modified code courtesy of https://www.freecodecamp.org/news/random-number-generator/
+function lcg(seed, multiplier, increment, modulus, length) {
+    const results = [];
+    for (let i = 0; i < length; i++) {
+        seed = (seed * multiplier + increment) % modulus;
+        results.push(seed / modulus); // divides by modulus for a value between 0 and 1
+    }
+    return results;
+};
+let lcgArrayLength = 1000;
+let lcgArray = lcg(58008, 16807, 0, 2147483647, lcgArrayLength);
+
+// defining the canvas outside of the function so i can mess with it a bit more
+// let main_canvas;
+
+// defining an offscreen buffer to use as a mask
+let buffer;
+
+function setup() {
+    let main_canvas = createCanvas(canvasWidth, canvasHeight);
     let r = random(100);
     main_canvas.parent('canvasContainer');
     frameRate(24 * buffersPerFrame);
+
     /* 
     just setting some stuff up for draw_one_frame.js shouldn't break anything dont yell at me k thx
     */
     // colorMode(HSB, 360, 100, 100, 100);
     colorMode(RGB);
-    // rectMode(CENTER);
+    rectMode(CENTER);
     noStroke();
     noSmooth(); // Disables antialising
+
+    buffer = createGraphics(canvasWidth, canvasHeight); // defining the offscreen buffer
+    buffer.colorMode(RGB);
+    buffer.rectMode(CENTER);
+    buffer.noStroke();
+    buffer.noSmooth();
 }
 
-function mousePressed(){
+function mousePressed() {
 }
 
-function draw () {
+function draw() {
     let animation_max_frames = frameMax * buffersPerFrame;
     let sticky_max_frames = animation_max_frames + stickFrame;
     let cur_frame = frameCount % sticky_max_frames;
@@ -47,30 +75,30 @@ function draw () {
         cur_frame = 0;
     }
     let cur_frac = map(cur_frame, 0, animation_max_frames, 0, 1);
-    
+
     background(debugZoomBackground);
-    
+
     push();
-    
-    if(debugZoom) {
+
+    if (debugZoom) {
         translate(0.5 * width, 0.5 * height);
         scale(debugZoomScale);
-        translate(0.5 * -width, 0.5 * -height);    
+        translate(0.5 * -width, 0.5 * -height);
     }
-    
+
     draw_one_frame(cur_frac);
-    
+
     pop();
-    
-    if(debugView) {
+
+    if (debugView) {
         textSize(28);
         fill(debugViewText);
-        text("" + (cur_frame/buffersPerFrame).toFixed(2) + " / " + 
-        (animation_max_frames/buffersPerFrame).toFixed(2) + " = " + 
-        cur_frac.toFixed(2), 50, 50);
+        text("" + (cur_frame / buffersPerFrame).toFixed(2) + " / " +
+            (animation_max_frames / buffersPerFrame).toFixed(2) + " = " +
+            cur_frac.toFixed(2), 50, 50);
     }
-    
-    if(recording) {
+
+    if (recording) {
         textSize(24);
         gifRecorder.addBuffer();
     }
@@ -99,9 +127,9 @@ function keyTyped() {
         stickFrame = 0;
     }
     if (key == 'r') {
-        if (recording==false){
+        if (recording == false) {
             recording = true;
-            gifRecorder = new p5recorder (frameMax, 'wallpaper.gif', buffersPerFrame);
-        }    
+            gifRecorder = new p5recorder(frameMax, 'wallpaper.gif', buffersPerFrame);
+        }
     }
 }
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,53 +1,71 @@
 let shapeList = [];
-let frameStep = 2;
-let frame = 0;
+let shapeLcgIndex = 0;
+let shapeLcgIndexOffset = 0;
 let colorStopList;
 
+let gradientShift = 0;
+
+// unused
+// let frameStep = 0;
+// let frame = 0;
+
+let colorLight = [159, 159, 159];
+let colorDark = [95, 95, 95];
+
+let firstFrame = true;
+
 function draw_one_frame() {
+    gradientShift += 1 / 12;
     colorStopList = [
-        [
-            {
-                position: 0,
-                color: [0, 0, 0]
-            },
-            {
-                position: 1,
-                color: [0, 0, 100]
-            },
-        ],
-        [
-            {
-                position: 0,
-                color: [219, 112, 121]
-            },
-            {
-                position: 0.5,
-                color: [185, 70, 109]
-            },
-            {
-                position: 1,
-                color: [27, 22, 29]
-            },
-        ],
+        {
+            position: 0,
+            color: colorDark
+        },
+        {
+            position: 0.2,
+            color: colorLight
+        },
+        {
+            position: 0.4,
+            color: colorDark
+        },
+        {
+            position: 0.6,
+            color: colorLight
+        },
+        {
+            position: 0.8,
+            color: colorDark
+        },
+        {
+            position: 1,
+            color: colorLight
+        },
     ]
     background(0);
-    // stroke(0);
-    // strokeWeight(2);
+
+    // generates shapes on startup
+    if (firstFrame) {   
+        for (let i=0;i<200;i++) {
+            addShape();
+        }
+    }
+    firstFrame = false;
+
     shapeList.forEach(element => {
         drawShape(element);
     });
     
-    if (frame < frameStep) {
-        frame++;
-    } else {
-        addShape();
-        if (shapeList.length > 50) {
-            shapeList.shift();
-        }
-        frame = 0;
-    }
-    blendMode(OVERLAY)
+    circle()
+    
+    buffer.background(0);
+    buffer.fill(255,95,127);
+    buffer.circle(canvasWidth/4,canvasHeight/4,300);
+    buffer.fill(0);
+    buffer.circle(canvasWidth/4,canvasHeight/4,150);
+    blendMode(OVERLAY);
     image(imageNoise,0,0,canvasWidth,canvasHeight,0,0,imageNoise.width,imageNoise.height,COVER);
+    drawingContext.drawImage(buffer.canvas, 0, 0);
 }
 
 function linearGradient(x1,y1,x2,y2,colorStops) {
@@ -57,34 +75,55 @@ function linearGradient(x1,y1,x2,y2,colorStops) {
     });
     return gradient;
 }
-function radialGradient(x1,y1,r1,x2,y2,r2,colorStops) {
-    let gradient = drawingContext.createRadialGradient(x1, y1, r1, x2, y2, r2);
-    colorStops.forEach(element => {
-        gradient.addColorStop(element.position, element.color);
-    });
-    return gradient;
+function returnLcg(fraction,total) {
+    return lcgArray[(shapeLcgIndex + shapeLcgIndexOffset + (Math.floor(lcgArrayLength * fraction / total))) % lcgArrayLength];
 }
+
+function addRing(x,y,w) {
+    buffer.circle
+}
+
 function addShape() {
-    let xColumns = 4;
-    let yRows = 3;
     let cornerRadii = 100;
     let sizeFactor = 100;
+    let spacingFactor = sizeFactor;
     let minSizeFactor = 2;
-    let maxSizeFactor = 4;
-    let spacingFactor = 200;
+    let maxSizeFactor = 3;
+    
+    let maxHorizontalShapes = Math.floor(canvasWidth / spacingFactor);
+    let maxVerticalShapes = Math.floor(canvasHeight / spacingFactor);
+    
+    let widthMultiplier = Math.floor(map(returnLcg(0,12), 0, 1, minSizeFactor, maxSizeFactor + 1));
+    let heightMultiplier = Math.floor(map(returnLcg(1,12), 0, 1, minSizeFactor, maxSizeFactor + 1));
+    
+    let width = widthMultiplier * spacingFactor;
+    let height = heightMultiplier * spacingFactor;
 
-    let xOffset = Math.floor(random()*xColumns) * spacingFactor;
-    let yOffset = Math.floor(random()*yRows) * spacingFactor;
-    let width = Math.round(map(random(),0,1,minSizeFactor,maxSizeFactor)) * sizeFactor;
-    let height = Math.round(map(random(),0,1,minSizeFactor,maxSizeFactor)) * sizeFactor;
-    let gradientChoice = Math.floor(random()*4);
-    let cornerRadiiChance = 0.67;
+    let widthExtensionDirection = Math.round(returnLcg(2,12)) * 2 - 1;
+    let heightExtensionDirection = Math.round(returnLcg(3,12)) * 2 - 1;
+
+    let xOffsetCanvasCenter = canvasWidth / 2
+    let xOffsetRandomGridPlacement = (Math.round(returnLcg(4,12) * (maxHorizontalShapes+1) - ((maxHorizontalShapes+1)/2))) * spacingFactor;
+    let xOffsetExtension = widthExtensionDirection * 0.5 * (widthMultiplier % 2 == 0 ? 1 : 0) * spacingFactor;
+    
+    let yOffsetCanvasCenter = canvasHeight / 2
+    let yOffsetRandomGridPlacement = (Math.round(returnLcg(5,12) * (maxVerticalShapes+1) - ((maxVerticalShapes+1)/2))) * spacingFactor;
+    let yOffsetExtension = heightExtensionDirection * 0.5 * (heightMultiplier % 2 == 0 ? 1 : 0) * spacingFactor;
+
+    let xOffset = xOffsetCanvasCenter + xOffsetRandomGridPlacement + xOffsetExtension;
+    let yOffset = yOffsetCanvasCenter + yOffsetRandomGridPlacement + yOffsetExtension;
+
+    let gradientChoice = Math.floor(returnLcg(6,12) * 4);
+    let cornerRadiiChance = 0.667;
     let cornerRadiiList = [
-        random() > cornerRadiiChance ? 0 : cornerRadii,
-        random() > cornerRadiiChance ? 0 : cornerRadii,
-        random() > cornerRadiiChance ? 0 : cornerRadii,
-        random() > cornerRadiiChance ? 0 : cornerRadii
+        returnLcg(7,12) > cornerRadiiChance ? 0 : cornerRadii,
+        returnLcg(8,12) > cornerRadiiChance ? 0 : cornerRadii,
+        returnLcg(9,12) > cornerRadiiChance ? 0 : cornerRadii,
+        returnLcg(10,12) > cornerRadiiChance ? 0 : cornerRadii
     ];
+
+    let gradientShiftDirection = Math.round(returnLcg(11,12)) * 2 - 1;
+
     shapeList.push(
         {
             x: xOffset,
@@ -92,25 +131,33 @@ function addShape() {
             w: width,
             h: height,
             cr: cornerRadiiList,
-            g: gradientChoice
+            g: gradientChoice,
+            gs: gradientShiftDirection
         }
     );
+
+    shapeLcgIndex++;
 }
 function drawShape(element) {
     push();
     let gradients = [
-        [0,0,1,0],
-        [0,0,0,1],
-        [1,0,0,0],
-        [0,1,0,0]
+        [-0.5, 0, 0.5, 0],
+        [0, -0.5, 0, 0.5],
+        [0.5, 0, -0.5, 0],
+        [0, 0.5, 0, -0.5]
     ]
-    let gradientsRandomChoice = gradients[element.g]
+    let gradientsRandomChoice = gradients[element.g];
+    let gradientScaleFactor = 5;
+
+    let gradientShiftHorizontal = (element.w * (gradientShift % 2 * element.gs));
+    let gradientShiftVertical = (element.h * (gradientShift % 2 * element.gs));
+
     drawingContext.fillStyle = linearGradient(
-        element.x + (element.w * gradientsRandomChoice[0]),
-        element.y + (element.h * gradientsRandomChoice[1]),
-        element.x + (element.w * gradientsRandomChoice[2]),
-        element.y + (element.h * gradientsRandomChoice[3]),
-        colorStopList[1]
+        element.x + (element.w * gradientsRandomChoice[0] * gradientScaleFactor + gradientShiftHorizontal),
+        element.y + (element.h * gradientsRandomChoice[1] * gradientScaleFactor + gradientShiftVertical),
+        element.x + (element.w * gradientsRandomChoice[2] * gradientScaleFactor + gradientShiftHorizontal),
+        element.y + (element.h * gradientsRandomChoice[3] * gradientScaleFactor + gradientShiftVertical),
+        colorStopList
     );
     rect(
         element.x,
@@ -121,6 +168,6 @@ function drawShape(element) {
         element.cr[1],
         element.cr[2],
         element.cr[3]
-        );
+    );
     pop();
 }
